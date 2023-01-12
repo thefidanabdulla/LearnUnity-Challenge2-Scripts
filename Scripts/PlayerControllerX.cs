ee0001fe433db70980600e6d8bd98f51a2b42b76
Galaxomas <thefidanabdulla@gmail.com>
2023-01-12 13:20:36 +0400

    learn unity junior programmer pathway challenge 2

---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float secondsBetweenShots = 1.0f;
+    private float nextShotTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var canShoot = nextShotTime <= 0;
+        nextShotTime -= Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canShoot)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            nextShotTime = secondsBetweenShots;
+        }
+    }
+}
